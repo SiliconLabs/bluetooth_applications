f3529fff7c4c960c80e110ae2c5c52df71b01430
nino <66275199+silabs-nino@users.noreply.github.com>
2021-06-07 15:50:47 -0500

    feature/bluetooth_rfid_example (#38)
    * adds bluetooth rfid example

---
+++ bluetooth_rfid_notify/src/app.c
@@ -0,0 +1,205 @@
+/***************************************************************************//**
+ * @file app.c
+ * @brief Bluetooth example project utilizing RFID Driver
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2021 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * SPDX-License-Identifier: Zlib
+ *
+ * The licensor of this software is Silicon Laboratories Inc.
+ *
+ * This software is provided \'as-is\', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ *******************************************************************************
+ * # Experimental Quality
+ * This code has not been formally tested and is provided as-is. It is not
+ * suitable for production environments. In addition, this code will not be
+ * maintained and there may be no bug maintenance planned for these resources.
+ * Silicon Labs may update projects from time to time.
+ ******************************************************************************/
+
+#include "em_common.h"
+#include "app_assert.h"
+#include "sl_bluetooth.h"
+#include "gatt_db.h"
+#include "app.h"
+
+// Third Party Includes
+#include "printf.h"
+
+// RFID Library includes
+#include "rfid.h"
+
+// RFID Static declarations
+RFID_DECLARE_RX_BUFFER;
+static rfid_uid_t rfid_uid;
+
+
+// The advertising set handle allocated from Bluetooth stack.
+static uint8_t advertising_set_handle = 0xff;
+
+/**************************************************************************//**
+ * Application Init.
+ *****************************************************************************/
+SL_WEAK void app_init(void)
+{
+  /////////////////////////////////////////////////////////////////////////////
+  // Put your additional application init code here!                         //
+  // This is called once during start-up.                                    //
+  /////////////////////////////////////////////////////////////////////////////
+
+  printf("A demo of the RFID driver to showcase Stack Compatibility!\r\n");
+
+  // rfid default initialization
+  rfid_init_t init = RFID_INIT_DEFAULT;
+  rfid_init(&init);
+
+  printf("[RFID]: Driver Initialized.\r\n");
+}
+
+/**************************************************************************//**
+ * Application Process Action.
+ *****************************************************************************/
+SL_WEAK void app_process_action(void)
+{
+  /////////////////////////////////////////////////////////////////////////////
+  // Put your additional application code here!                              //
+  // This is called infinitely.                                              //
+  // Do not call blocking functions from here!                               //
+  /////////////////////////////////////////////////////////////////////////////
+
+  sl_status_t status;
+
+  // check for new data received
+  status = rfid_process_action();
+  if(status == SL_STATUS_OK) {
+      // copy uid from handle to rfid_uid
+      rfid_get_data(&rfid_uid);
+
+      // a valid card was read, send notification
+      if(rfid_uid.valid) {
+          printf("[RFID]: Read new card! :)\r\n");
+          status = sl_bt_gatt_server_notify_all(gattdb_card_uid, 10, (const uint8_t *) rfid_uid.card_uid);
+          app_assert(status == SL_STATUS_OK,
+                        "[E: 0x%04X] Failed to send notification\r\n",
+                        (int)status);
+      }
+  } else if(status == SL_STATUS_INVALID_COUNT) {
+      // received a checksum error
+      printf("[RFID]: Checksum error :c\r\n");
+  }
+}
+
+/**************************************************************************//**
+ * Bluetooth stack event handler.
+ * This overrides the dummy weak implementation.
+ *
+ * @param[in] evt Event coming from the Bluetooth stack.
+ *****************************************************************************/
+void sl_bt_on_event(sl_bt_msg_t *evt)
+{
+  sl_status_t sc;
+  bd_addr address;
+  uint8_t address_type;
+  uint8_t system_id[8];
+
+  switch (SL_BT_MSG_ID(evt->header)) {
+    // -------------------------------
+    // This event indicates the device has started and the radio is ready.
+    // Do not call any stack command before receiving this boot event!
+    case sl_bt_evt_system_boot_id:
+
+      // Extract unique ID from BT Address.
+      sc = sl_bt_system_get_identity_address(&address, &address_type);
+      app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to get Bluetooth address\n",
+                    (int)sc);
+
+      // Pad and reverse unique ID to get System ID.
+      system_id[0] = address.addr[5];
+      system_id[1] = address.addr[4];
+      system_id[2] = address.addr[3];
+      system_id[3] = 0xFF;
+      system_id[4] = 0xFE;
+      system_id[5] = address.addr[2];
+      system_id[6] = address.addr[1];
+      system_id[7] = address.addr[0];
+
+      sc = sl_bt_gatt_server_write_attribute_value(gattdb_system_id,
+                                                   0,
+                                                   sizeof(system_id),
+                                                   system_id);
+      app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to write attribute\n",
+                    (int)sc);
+
+      // Create an advertising set.
+      sc = sl_bt_advertiser_create_set(&advertising_set_handle);
+      app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to create advertising set\n",
+                    (int)sc);
+
+      // Set advertising interval to 100ms.
+      sc = sl_bt_advertiser_set_timing(
+        advertising_set_handle,
+        160, // min. adv. interval (milliseconds * 1.6)
+        160, // max. adv. interval (milliseconds * 1.6)
+        0,   // adv. duration
+        0);  // max. num. adv. events
+      app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to set advertising timing\n",
+                    (int)sc);
+      // Start general advertising and enable connections.
+      sc = sl_bt_advertiser_start(
+        advertising_set_handle,
+        advertiser_general_discoverable,
+        advertiser_connectable_scannable);
+      app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to start advertising\n",
+                    (int)sc);
+      break;
+
+    // -------------------------------
+    // This event indicates that a new connection was opened.
+    case sl_bt_evt_connection_opened_id:
+      break;
+
+    // -------------------------------
+    // This event indicates that a connection was closed.
+    case sl_bt_evt_connection_closed_id:
+      // Restart advertising after client has disconnected.
+      sc = sl_bt_advertiser_start(
+        advertising_set_handle,
+        advertiser_general_discoverable,
+        advertiser_connectable_scannable);
+      app_assert(sc == SL_STATUS_OK,
+                    "[E: 0x%04x] Failed to start advertising\n",
+                    (int)sc);
+      break;
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Add additional event handlers here as your application requires!      //
+    ///////////////////////////////////////////////////////////////////////////
+
+    // -------------------------------
+    // Default event handler.
+    default:
+      break;
+  }
+}
