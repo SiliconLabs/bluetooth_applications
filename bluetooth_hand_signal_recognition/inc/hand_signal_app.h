c1c8eafe43dc6c5baafdee9f8599affbb3a365df
HopNguyen <76993461+silabs-HopN@users.noreply.github.com>
2023-02-03 16:10:22 +0700

    Add ble_hand_signal_recognition project

---
+++ bluetooth_hand_signal_recognition/inc/hand_signal_app.h
@@ -0,0 +1,95 @@
+/***************************************************************************//**
+ * @file hand_signal_app.h
+ * @brief hand signal application file.
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2022 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * SPDX-License-Identifier: Zlib
+ *
+ * The licensor of this software is Silicon Laboratories Inc.
+ *
+ * This software is provided \'as-is\', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented{} you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ *******************************************************************************
+ * # Experimental Quality
+ * This code has not been formally tested and is provided as-is. It is not
+ * suitable for production environments. In addition, this code will not be
+ * maintained and there may be no bug maintenance planned for these resources.
+ * Silicon Labs may update projects from time to time.
+ ******************************************************************************/
+#ifndef HAND_SIGNAL_APP_H
+#define HAND_SIGNAL_APP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include "sl_status.h"
+
+#define TFLITE_MODEL_CLASS_COUNT 3
+
+// These are parameters that are optionally embedded
+// into the .tflite model file
+typedef struct AppSettings
+{
+  // norm_img = img * rescale
+  float samplewise_norm_rescale;
+  // norm_img = (img - mean(img)) / std(img)
+  bool samplewise_norm_mean_and_std;
+  // enable verbose inference logging
+  bool verbose_inference_output;
+  // Drop all inference results older than this value
+  uint32_t average_window_duration_ms;
+  // The minimum number of inference results to average
+  uint32_t minimum_count;
+  // Minimum averaged model output threshold for a class
+  // to be considered detected, 0-255; 255 = highest confidence
+  uint8_t detection_threshold;
+  // The number of samples that are different than the last detected sample
+  // for a new detection to occur
+  uint32_t suppression_count;
+  // This the amount of time in milliseconds between inference
+  uint32_t inference_time;
+} app_settings_t;
+
+// Initializes all data needed for the example.
+sl_status_t hand_signal_recognition_init(void);
+
+// Runs one iteration of data gathering and inference. This should be called
+// repeatedly from the application code.
+void hand_signal_recognition_loop(void);
+
+/***************************************************************************//**
+ * Get the label for a certain category/class
+ *
+ * @param index
+ *   index of the category/class
+ *
+ * @return
+ *   pointer to the label string. The label is "?" if no corresponding label
+ *   was found.
+ ******************************************************************************/
+const char *get_category_label(int index);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // HAND_SIGNAL_APP_H
---
+++ bluetooth_hand_signal_recognition/src/output_handler.c
@@ -0,0 +1,129 @@
+/***************************************************************************//**
+ * @file output_handler.cc
+ * @brief output handler file.
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2022 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * SPDX-License-Identifier: Zlib
+ *
+ * The licensor of this software is Silicon Laboratories Inc.
+ *
+ * This software is provided \'as-is\', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented{} you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ *******************************************************************************
+ * # Experimental Quality
+ * This code has not been formally tested and is provided as-is. It is not
+ * suitable for production environments. In addition, this code will not be
+ * maintained and there may be no bug maintenance planned for these resources.
+ * Silicon Labs may update projects from time to time.
+ ******************************************************************************/
+#include "sl_led.h"
+#include "sl_simple_led_instances.h"
+
+#include "gatt_db.h"
+
+#include "app_log.h"
+#include "app_assert.h"
+
+#include "hand_signal_app.h"
+#include "hand_signal_display.h"
+#include "output_handler.h"
+
+static bool notifications_enabled = false;
+// The format for hand_signal characteristic: <detect_class_id> <score>
+static uint8_t data[2];
+
+/***************************************************************************//**
+ * Handle inference result
+ ******************************************************************************/
+void handle_result(int32_t current_time,
+                   uint8_t result,
+                   uint8_t score,
+                   bool is_new_command)
+{
+  sl_status_t sc;
+  const char *label = get_category_label(result);
+
+  sl_led_toggle(&sl_led_led0);
+
+  if (notifications_enabled) {
+    sc = sl_bt_gatt_server_notify_all(gattdb_hand_signal,
+                                      sizeof(data),
+                                      data);
+    app_assert_status(sc);
+  }
+
+  if (is_new_command) {
+    // Update data for ble
+    data[0] = result;
+    data[1] = score;
+    hand_signal_display_update(result, score);
+    app_log("Detected class=%d label=%s score=%d @%ldms\n",
+            result,
+            label,
+            score,
+            current_time);
+  }
+}
+
+/*******************************************************************************
+ * @brief
+ *   Function to handle evt_gatt_server_characteristic_status_id event
+ ******************************************************************************/
+void hand_signal_characteristic_status(sl_bt_msg_t *evt)
+{
+  // Notification or Indication status changed for Hand signal
+  if (evt->data.evt_gatt_server_characteristic_status.status_flags
+      == gatt_server_client_config) {
+    notifications_enabled
+      = evt->data.evt_gatt_server_characteristic_status.client_config_flags;
+  }
+}
+
+/*******************************************************************************
+ * @brief
+ *   Function to handle sl_bt_evt_gatt_server_user_read_request_id event
+ ******************************************************************************/
+void hand_signal_read_requests(sl_bt_msg_t *evt)
+{
+  sl_status_t sc;
+  uint16_t data_len;
+
+  if (evt->data.evt_gatt_server_user_read_request.characteristic
+      == gattdb_hand_signal) {
+    // Send response
+    sc = sl_bt_gatt_server_send_user_read_response(
+      evt->data.evt_gatt_server_user_read_request.connection,
+      evt->data.evt_gatt_server_user_read_request.characteristic,
+      SL_STATUS_OK, /* SUCCESS */
+      sizeof(data), data, &data_len);
+    app_assert_status(sc);
+  }
+}
+
+/*******************************************************************************
+ * @brief
+ *   Function to handle disconnect event
+ ******************************************************************************/
+void hand_signal_disconnect_event(sl_bt_msg_t *evt)
+{
+  (void)evt;
+
+  notifications_enabled = false;
+}
